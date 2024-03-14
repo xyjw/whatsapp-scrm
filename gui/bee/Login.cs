a1637252f301071dda08d60d2ca85dc64b37d973
yongxiangHuang <30454129+xyjw@users.noreply.github.com>
2024-03-14 17:26:33 +0800

    Add files via upload

---
+++ gui/bee/Login.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Collections;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace bee
+{
+    public partial class Login : Form
+    {
+        public static string username;
+        public static string team;
+        // 设置运行模式
+        public static int runMode = 0;
+        Models m = new Models();
+        public Login()
+        {
+            InitializeComponent();
+            Hashtable res = sendConfig("read");
+            this.usernameText.Text = res["userName"].ToString();
+            this.passwordText.Text = res["passWord"].ToString();
+            m.runMode = runMode;
+            // 设置按钮状态
+            if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "driver.txt")))
+            {
+                this.loginButton.Enabled = false;
+                this.clearButton.Enabled = false;
+            }
+        }
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            username = this.usernameText.Text;
+            string password = this.passwordText.Text;
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("请输入账号密码！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            // 数据查询验证用户信息
+            string sql = $"select * from account where account='{username}' and password='{password}'";
+            List<Hashtable> res = m.QueryDB(sql, "account");
+            if (res.Count > 0)
+            {
+                team = res[0]["team"].ToString();
+                sendConfig("modify", username, password);
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                this.tips.Text = "账号密码异常";
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            this.usernameText.Text = "";
+            this.passwordText.Text = "";
+            this.tips.Text = "";
+            sendConfig("clear");
+        }
+
+        private Hashtable sendConfig(string mode, string userName="", string passWord="")
+        {
+            var configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configuration.AppSettings.Settings;
+            Hashtable res = new Hashtable();
+            res["userName"] = "";
+            res["passWord"] = "";
+            if (mode == "read")
+            {
+                if (settings["userName"] != null)
+                {
+                    res["userName"] = settings["userName"].Value;
+                    res["passWord"] = settings["passWord"].Value;
+                }
+            }
+            else
+            {
+                if (settings["userName"] != null)
+                {
+                    settings["userName"].Value = userName;
+                    settings["passWord"].Value = passWord;
+                }
+                else
+                {
+                    settings.Add("userName", userName);
+                    settings.Add("passWord", passWord);
+                }
+                configuration.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configuration.AppSettings.SectionInformation.Name);
+            }
+            return res;
+        }
+
+        private void initializeButton_Click(object sender, EventArgs e)
+        {
+            this.initializeButton.Enabled = false;
+            this.loginButton.Enabled = false;
+            this.clearButton.Enabled = false;
+            if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "driver.txt")))
+            {
+                File.Delete(Path.Combine(Directory.GetCurrentDirectory(), "driver.txt"));
+            }
+            System.Diagnostics.Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "getDriver.exe"));
+            Task task = new Task(() =>
+            {
+                while (true)
+                {
+                    if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "driver.txt")))
+                    {
+                        this.loginButton.Enabled = true;
+                        this.clearButton.Enabled = true;
+                        this.initializeButton.Enabled = true;
+                        break;
+                    }
+                    Thread.Sleep(5000);
+                }
+            });
+            task.Start();
+        }
+
+    }
+}
---
+++ gui/bee/Models.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
+namespace bee
+{
+    class Models
+    {
+        MySqlConnection conn;
+        string sql_ip = "", sql_port = "", sql_user = "", sql_pw = "", sql_name = "";
+        public int rm = 0;
+        public int runMode
+        {
+            get
+            {
+                return rm;
+            }
+            set
+            {
+                rm = value;
+            }
+           
+        }
+        
+        private void load()
+        {
+            if (runMode == 0) {
+                sql_ip = "127.0.0.1";  //IP
+                sql_port = "3306"; //端口号
+                sql_user = "root";  //用户名  
+                sql_pw = "1234";  //密码
+                sql_name = "whatsapp"; //数据库名
+            }
+            if (runMode == 1)
+            {
+                sql_ip = "192.168.10.152";  //IP
+                sql_port = "5050"; //端口号
+                sql_user = "root";  //用户名  
+                sql_pw = "kbl1234";  //密码
+                sql_name = "whatsapp1"; //数据库名
+            }
+            if (runMode == 2)
+            {
+                sql_ip = "81.71.13.163";  //IP
+                sql_port = "5050"; //端口号
+                sql_user = "root";  //用户名  
+                sql_pw = "QAZwsx1987!";  //密码
+                sql_name = "whatsapp"; //数据库名
+            }
+            if (sql_ip != "")
+            {
+                string ConnectionString = $"server={sql_ip};user={sql_user};password ={sql_pw};database={sql_name}; port={sql_port};persistsecurityinfo = True;SslMode=None ;Charset=utf8mb4;";
+                conn = new MySqlConnection(ConnectionString);
+                conn.Open();
+            }
+            
+        }
+        private void close()
+        {
+            conn.Close();
+        }
+        public List<Hashtable> QueryDB(string sql, string form)
+        {
+            load();
+            List<Hashtable> res = new List<Hashtable>();
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            // 执行命令并获取读取器
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Hashtable d = new Hashtable();
+                if (form == "account")
+                {
+                    d.Add("account", reader.GetString("account"));
+                    d.Add("password", reader.GetString("password"));
+                    d.Add("team", reader.GetString("team"));
+                }
+                if (form == "customer")
+                {
+                    d.Add("id", reader.GetString("id"));
+                    d.Add("phone", reader.GetString("phone"));
+                    d.Add("customername", reader.GetString("customername"));
+                    d.Add("labels1", reader.GetString("labels1"));
+                    d.Add("labels2", reader.GetString("labels2"));
+                    d.Add("labels3", reader.GetString("labels3"));
+                    d.Add("labels4", reader.GetString("labels4"));
+                    d.Add("labels5", reader.GetString("labels5"));
+                    d.Add("status", reader.GetString("status"));
+                    d.Add("interval", reader.GetString("interval"));
+                }
+                if (form == "action")
+                {
+                    d.Add("action", reader.GetString("action"));
+                }
+                if (form == "record")
+                {
+                    d.Add("id", reader.GetString("id"));
+                }
+                if (form == "label")
+                {
+                    d.Add("content", reader.GetString("content"));
+                }
+                res.Add(d);
+            }
+            close();
+            return res;
+        }
+        public void ExecuteDB(string sql)
+        {
+            load();
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            close();
+        }
+
+    }
+}
